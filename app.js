59d6e66d9fca9e95ffcb74ac76075e8874cce709
Mahik <mahik09@outlook.com>
2024-07-25 15:54:31 +0100

    tickets completed

--- app.js
+++ app.js
@@ -30,25 +30,86 @@ res.json({
 /* Write a request handler to return the correct response when a `GET` request is received to `/astronauts`. Choose the appropriate 
 function from the imported functions at the top of the `app.js` to get your data. */
 
+app.get("/astronauts", async (req, res) => {
+  try {
+    const astros = await getAstronauts();
+    res.json({
+      success: true,
+      payload: astros,
+    });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+});
+
 // Task 2
 
 /* Write a request handler to return the correct response and perform the correct action when a `POST` request is received to 
 `/astronauts`. Choose the appropriate function from the imported functions at the top of the `app.js` to perform the action. */
+
+app.post("/astronauts", async (req, res) => {
+  try {
+    const data = await createAstronaut(req.body);
+    res.json({
+      success: true,
+      payload: data,
+    });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+});
 
 // Task 3
 
 /* Write the request handler to return the data from the function getAstronautById. Have this handler listen to requests at the 
 appropriate path. */
 
+app.get("/astronauts/:id", async (req, res) => {
+  try {
+    // const userid = req.param.id
+    const astros = await getAstronautById(req.params.id);
+    res.json({
+      success: true,
+      payload: astros,
+    });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+});
+
 // Task 4
 
 /* Write the request handler to perform the action and return the data from the function replaceAstronautById. Have this handler 
 listen to requests at the appropriate path. */
 
+app.put("/astronauts/:id", async (req, res) => {
+  try {
+    const astros = await replaceAstronautById(req.params.id, req.body);
+    res.json({
+      success: true,
+      payload: astros,
+    });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+});
+
 // Task 5
 
 /* Write the request handler to perform the action and return the data from the function deleteAstronautById. Have this handler 
 listen to requests at the appropriate path. */
+
+app.delete("/astronauts/:id", async (req, res) => {
+  try {
+    const astros = await deleteAstronautById(req.params.id);
+    res.json({
+      success: true,
+      payload: astros,
+    });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+});
 
 // Task 6
 
